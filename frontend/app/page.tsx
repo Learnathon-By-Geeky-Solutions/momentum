ab1ed8a50c2749b8ecdf12427c2cd73c8d9ad026
Emdadul Islam <emdadulislam580@gmail.com>
2025-02-12 00:50:32 +0600

    feat(hero): add hero section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 
+import Hero from "@/components/brand/hero"
 import { MainHeader } from "@/components/brand/navbar/main-header"
 import { Navigation } from "@/components/brand/navbar/navigation"
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <MainHeader />
       <Navigation />
+      <Hero/>
     </div>
   )
 }
