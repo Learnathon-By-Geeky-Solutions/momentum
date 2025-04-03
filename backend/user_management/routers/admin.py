45acaffbec445116d07fc8f3343b6f20f3a0263c
ShajjadGani <shajjadg47@gmail.com>
2025-04-03 06:14:16 +0600

    feat: allow authorized admins to upgrade customer to admin

--- backend/user_management/routers/admin.py
+++ backend/user_management/routers/admin.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
+#Implement admin verification system for secure access control
 def get_current_admin(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     payload = verify_token(token)
     user_email = payload.get("sub")
@@ -16,3 +17,33 @@ def get_current_admin(token: str = Depends(oauth2_scheme), db: Session = Depends
     if not user or user.role != 'admin':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
     return user
+
+class PromoteUser(BaseModel):
+    role: str
+
+#Allow authorized admins to upgrade customer to admin
+@router.put("/users/promote/{user_id}")
+async def promote_user(
+    user_id: int,
+    role_data: PromoteUser,
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(get_current_admin)
+):
+    
+    role = role_data.role.lower()
+    
+    if role_data.role != 'admin':
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid role. Only 'admin' is allowed."
+        )
+    
+    user = db.query(User).filter(User.user_id == user_id).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    user.role = role_data.role
+    db.commit()
+    db.refresh(user)
+    return {"detail": f"User {user.username} is now an admin"}
